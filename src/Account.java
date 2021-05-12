399336575f59e974754c4fd37ac18a92988ceeca
Mai Nguyen Thanh Binh <mntbinh.99@gmail.com>
2021-05-12 11:09:16 +0700

    ATM

---
+++ src/Account.java
@@ -0,0 +1,38 @@
+public class Account {
+    private int numberAccount = 0;
+
+    public synchronized void add(int num){
+        if (numberAccount == 0){
+
+        }
+        numberAccount += num;
+        System.out.println("Đã nộp vào " + num);
+        notify();
+    }
+
+    public synchronized void sub(int num){
+        /*if (numberAccount > 50000){
+            numberAccount -= num;
+            System.out.println("Đã rút " + num);
+
+        }
+        else{
+            System.out.println("Số dư phải lớn hơn 50000!");
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }*/
+        if (numberAccount < 50000){
+            System.out.println("Số dư phải lớn hơn 50000!");
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        numberAccount -= num;
+        System.out.println("Đã rút " + num);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Account acc = new Account();
+
+        new Thread(()->{
+            acc.sub(50000);
+        }).start();
+
+        new Thread(()->{
+            acc.add(100000);
+        }).start();
+    }
+}
